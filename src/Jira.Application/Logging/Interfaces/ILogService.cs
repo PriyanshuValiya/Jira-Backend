6c57b79dc8e453bd918ba8328b10bb38aa6447cb
PriyanshuValiya <valiyapriyansukumar@gmail.com>
2026-07-16 12:17:38 +0530

    updated file

--- src/Jira.Application/Logging/Interfaces/ILogService.cs
+++ src/Jira.Application/Logging/Interfaces/ILogService.cs
@@ -3,6 +3,5 @@ namespace Jira.Application.Logging.Interfaces;
 public interface ILogService
 {
     Task<PagedLogResponse> GetLogsAsync(LogQueryRequest request);
-
     Task<List<LogGroupByIpResponse>> GroupByIpAsync(int? threshold);
 }
